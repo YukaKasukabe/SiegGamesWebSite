7ef8e6b9b830543749193337881a80dc7986030a
YukaKasukabe <kasukabe@sieggames.net>
2023-01-27 09:59:39 +0900

    更新

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 // 全画面表示
-const setFillHeight = () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
+// const setFillHeight = () => {
+//   const vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--vh', `${vh}px`);
+// }
 
-// 画面のサイズ変動があった時に高さを再計算する
-window.addEventListener('resize', setFillHeight);
+// // 画面のサイズ変動があった時に高さを再計算する
+// window.addEventListener('resize', setFillHeight);
 
-// 初期化
-setFillHeight();
+// // 初期化
+// setFillHeight();
 /* ------------------------------------------------------------------ */
 
 
