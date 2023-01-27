d3707279e6d353ef53d318047418afa02d436728
YukaKasukabe <kasukabe@sieggames.net>
2023-01-27 14:32:29 +0900

    更新

--- js/index.js
+++ js/index.js
@@ -1,36 +1,14 @@
-// 全画面表示
-// const setFillHeight = () => {
-//   const vh = window.innerHeight * 0.01;
-//   document.documentElement.style.setProperty('--vh', `${vh}px`);
-// }
+全画面表示
+const setFillHeight = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
 
-// // 画面のサイズ変動があった時に高さを再計算する
-// window.addEventListener('resize', setFillHeight);
+// 画面のサイズ変動があった時に高さを再計算する
+window.addEventListener('resize', setFillHeight);
 
-// // 初期化
-// setFillHeight();
-/* ------------------------------------------------------------------ */
-
-
-//スプラッシュロゴアニメーション
-$(window).on('load',function(){
-  $(".splash").delay(200).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-  $(".splash-logo").delay(200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
-});
-//=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-$(".splash").delay(1500).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
-
-  $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
-  
-  });
-  //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-  
-  //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
-  $('.splashbg').on('animationend', function() { 
-  //この中に動かしたいJSを記載
-  });
-  //=====ここまで背景が伸びた後に動かしたいJSをまとめる
-
+// 初期化
+setFillHeight();
 /* ------------------------------------------------------------------ */
 
 //ページネーション
