f8447f8e820eae738c71db93a12b0ed7a60f8262
YukaKasukabe <41414756+YukaKasukabe@users.noreply.github.com>
2023-01-29 00:25:50 +0900

    Add files via upload
    更新

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ $.scrollify({
   scrollbars:"false",//スクロールバー表示・非表示設定
   interstitialSection : ".footer",
   easing: "swing", // swing,linear,easeOutExpo
-  scrollSpeed: 1000, // スクロール時の速度
-  standardScrollElements: '.index-section2,..index-section3,.index-section4',
+  scrollSpeed: 800, // スクロール時の速度
+  standardScrollElements: 'index-section2,index-section3,index-section4',
   
   //以下、ページネーション設定
   before:function(i,panels) {
