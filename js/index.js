82bfb8ee54cf85b24b62e1a43230abbb5782fce1
YukaKasukabe <kasukabe@sieggames.net>
2023-01-30 14:30:00 +0900

    モバイル対応

--- js/index.js
+++ js/index.js
@@ -1,39 +1,74 @@
-$.scrollify({
-  section : ".index-section",//1ページスクロールさせたいエリアクラス名
-  scrollbars:"false",//スクロールバー表示・非表示設定
-  interstitialSection : ".footer",
-  easing: "swing", // swing,linear,easeOutExpo
-  scrollSpeed: 1100, // スクロール時の速度
-  standardScrollElements: 'index-section2,index-section3,index-section4',
-  // setHeights: true,
-  // overflowScroll: true,
-  // updateHash: true,
-  // touchScroll:true,
 
-  
-  //以下、ページネーション設定
-  before:function(i,panels) {
-    var ref = panels[i].attr("data-section-name");
-      $(".pagination .active").removeClass("active");
-      $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
-    },
-    afterRender:function() {
-      var pagination = "<ul class=\"pagination\">";
-      var activeClass = "";
-      $(".index-section").each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
-        activeClass = "";
-        if(i===$.scrollify.currentIndex()) {
-          activeClass = "active";
+if(!navigator.userAgent.match(/(iPhone|Android)/)){
+  $(function(){
+    $.scrollify({
+      section : ".index-section",//1ページスクロールさせたいエリアクラス名
+      scrollbars : "false",//スクロールバー表示・非表示設定
+      interstitialSection : ".footer",
+      easing : "swing", // swing,linear,easeOutExpo
+      scrollSpeed : 800, // スクロール時の速度
+      
+      //以下、ページネーション設定
+      before:function(i,panels) {
+        var ref = panels[i].attr("data-section-name");
+          $(".pagination .active").removeClass("active");
+          $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+        },
+        afterRender:function() {
+          var pagination = "<ul class=\"pagination\">";
+          var activeClass = "";
+          $(".index-section").each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
+            activeClass = "";
+            if(i===$.scrollify.currentIndex()) {
+              activeClass = "active";
+            }
+            pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+          });
+          pagination += "</ul>";
+    
+          $("#index-section1").append(pagination);//はじめのエリアにページネーションを表示
+          $(".pagination a").on("click",$.scrollify.move);
         }
-        pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+    
       });
-      pagination += "</ul>";
-
-      $("#index-section1").append(pagination);//はじめのエリアにページネーションを表示
-      $(".pagination a").on("click",$.scrollify.move);
-    }
-
   });
+  }
+
+  if(navigator.userAgent.match(/(iPhone|iPad|Android)/)){
+    $(function(){
+      $.scrollify({
+        section : ".index-section",//1ページスクロールさせたいエリアクラス名
+        scrollbars : "false",//スクロールバー表示・非表示設定
+        interstitialSection : ".footer",
+        easing : "swing", // swing,linear,easeOutExpo
+        scrollSpeed : 1100, // スクロール時の速度
+        standardScrollElements: '#index-section1,#index-section2,#index-section3,#index-section4',
+        
+        //以下、ページネーション設定
+        // before:function(i,panels) {
+        //   var ref = panels[i].attr("data-section-name");
+        //     $(".pagination .active").removeClass("active");
+        //     $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+        //   },
+        //   afterRender:function() {
+        //     var pagination = "<ul class=\"pagination\">";
+        //     var activeClass = "";
+        //     $(".index-section").each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
+        //       activeClass = "";
+        //       if(i===$.scrollify.currentIndex()) {
+        //         activeClass = "active";
+        //       }
+        //       pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+        //     });
+        //     pagination += "</ul>";
+      
+        //     $("#index-section1").append(pagination);//はじめのエリアにページネーションを表示
+        //     $(".pagination a").on("click",$.scrollify.move);
+        //   }
+      
+        });
+    });
+    }
 
   $(window).on('resize',function(){
     if(current){
