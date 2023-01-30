fedfe1f4263f9abc4ef8b61914dace0d920f8d47
YukaKasukabe <kasukabe@sieggames.net>
2023-01-30 14:41:53 +0900

    モバイル対応

--- js/index.js
+++ js/index.js
@@ -42,30 +42,7 @@ if(!navigator.userAgent.match(/(iPhone|Android)/)){
         interstitialSection : ".footer",
         easing : "swing", // swing,linear,easeOutExpo
         scrollSpeed : 1100, // スクロール時の速度
-        standardScrollElements: '#index-section1,#index-section2,#index-section3,#index-section4',
-        
-        //以下、ページネーション設定
-        // before:function(i,panels) {
-        //   var ref = panels[i].attr("data-section-name");
-        //     $(".pagination .active").removeClass("active");
-        //     $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
-        //   },
-        //   afterRender:function() {
-        //     var pagination = "<ul class=\"pagination\">";
-        //     var activeClass = "";
-        //     $(".index-section").each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
-        //       activeClass = "";
-        //       if(i===$.scrollify.currentIndex()) {
-        //         activeClass = "active";
-        //       }
-        //       pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
-        //     });
-        //     pagination += "</ul>";
-      
-        //     $("#index-section1").append(pagination);//はじめのエリアにページネーションを表示
-        //     $(".pagination a").on("click",$.scrollify.move);
-        //   }
-      
+        standardScrollElements: '#index-section2,#index-section3,#index-section4',
         });
     });
     }
